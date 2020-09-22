75964698d0d807fdbc12df93329d1205acfdde9c
Dasha Lary <dk2800@nyu.edu>
2020-09-22 18:01:06 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Ruby Collaborating Objects Lab
+tI's# Ruby Collaborating Objects Lab
 
 ## Objective
 
---
+++ lib/mp3_importer.rb
@@ -0,0 +1,20 @@
+class MP3Importer 
+attr_accessor :path
+  
+  def initialize(path)
+  @path = path 
+  end 
+  
+  def files
+  Dir.entries(path).select { |f| f.end_with?("mp3") }
+  end
+  
+  def import
+  files.each{ |filename| Song.new_by_filename(filename) }
+    
+  end
+  
+  
+  
+  
+end

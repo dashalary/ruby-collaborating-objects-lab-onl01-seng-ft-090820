2c3de68bfd33b1058b70ed0b22ece52aa337c5ba
Dasha Lary <dk2800@nyu.edu>
2020-09-22 17:50:31 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,7 +6,7 @@ attr_accessor :path
   end 
   
   def files
-  Dir.entries(path).select { |f| f.end_with?("mp3") }
+  files = Dir.entries(path).select { |f| f.end_with?("mp3") }
   end
   
   def import
